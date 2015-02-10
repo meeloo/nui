2b713a6e746a0b541b858ce0dd9e9bee5ee78542
vince <vincent.bongiorno@intua.net>
2015-02-10 11:20:35 +0100

    commenting nuiLayer for now

--- include/nuiLayer.h
+++ include/nuiLayer.h
@@ -14,11 +14,11 @@ public:
   static nuiLayer* GetLayer(const nglString& rName);
   static nuiLayer* CreateLayer(const nglString& rName, int width, int height, nglImagePixelFormat PixelFormat = eImagePixelRGBA, int depth = 0, int stencil = 0);
   
-  NUI_GETSETDO(int, Width, Change());
-  NUI_GETSETDO(int, Height, Change());
-  NUI_GETSETDO(nglImagePixelFormat, PixelFormat, Change());
-  NUI_GETSETDO(int, Depth, Change());
-  NUI_GETSETDO(int, Stencil, Change());
+//  NUI_GETSETDO(int, Width, Change());
+//  NUI_GETSETDO(int, Height, Change());
+//  NUI_GETSETDO(nglImagePixelFormat, PixelFormat, Change());
+//  NUI_GETSETDO(int, Depth, Change());
+//  NUI_GETSETDO(int, Stencil, Change());
   
 private:
   nglString& mName;
