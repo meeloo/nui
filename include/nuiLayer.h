ea7e3a481ab12ef458fa47f3fc9ee6b3c1e7e935
Sebastien Metrot <meeloo@meeloo.net>
2015-02-22 12:28:47 +0100

    wip

--- include/nuiLayer.h
+++ include/nuiLayer.h
@@ -14,18 +14,23 @@ public:
   static nuiLayer* GetLayer(const nglString& rName);
   static nuiLayer* CreateLayer(const nglString& rName, int width, int height, nglImagePixelFormat PixelFormat = eImagePixelRGBA, int depth = 0, int stencil = 0);
   
-  NUI_GETSETDO(int, Width, Change());
-  NUI_GETSETDO(int, Height, Change());
+  NUI_GETSETDO(nuiRect, Frame, Change());
   NUI_GETSETDO(nglImagePixelFormat, PixelFormat, Change());
   NUI_GETSETDO(int, Depth, Change());
   NUI_GETSETDO(int, Stencil, Change());
-  
+
+  void AddChild(nuiLayer* pLayer);
+  void DelChild(nuiLayer* pLayer);
+  const std::vector<nuiLayer*>& GetChildren() const;
 private:
+  std::vector<nuiLayer*> mLayers;
+  void Change() { mChanged = true; }
+  bool mChanged = true;
   nglString& mName;
-  int mWidth = 0;
-  int mHeight = 0;
+  nuiRect mFrame;
+  nuiVector2 mAnchor;
   nglImagePixelFormat PixelFormat;
   int mDepth = 0;
   int mStencil = 0;
-  bool mChanged = true;
 };
+
