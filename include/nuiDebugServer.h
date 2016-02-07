8dead04787e9bd4d878c1a4a2332475cf5781750
Sebastien Metrot <meeloo@meeloo.net>
2016-02-07 23:11:33 +0100

    moving things around

--- include/nuiDebugServer.h
+++ include/nuiDebugServer.h
@@ -28,209 +28,23 @@ enum nuiMessageDataType
 
 struct nuiMessageData
 {
-  nuiMessageData(const nglString& rString)
-  {
-    mType = nuiMessageDataTypeString;
-    if (rString.IsNull())
-    {
-      mValue.Pointer._ptr = new uint8[rString.GetLength()];
-      mValue.Pointer._size = rString.GetLength();
-      memcpy(mValue.Pointer._ptr, rString.GetChars(), mValue.Pointer._size);
-    }
-    else
-    {
-      mValue.Pointer._ptr = nullptr;
-      mValue.Pointer._size = 0;
-    }
-  }
-
-  nuiMessageData(const void* ptr, size_t size)
-  {
-    mType = nuiMessageDataTypeBuffer;
-    if (ptr)
-    {
-      mValue.Pointer._ptr = new uint8[size];
-      mValue.Pointer._size = size;
-      memcpy(mValue.Pointer._ptr, ptr, size);
-    }
-    else
-    {
-      mValue.Pointer._ptr = nullptr;
-      mValue.Pointer._size = 0;
-    }
-  }
-
-  nuiMessageData(int8 value)
-  {
-    mType = nuiMessageDataTypeInt8;
-    mValue._int8 = value;
-  }
-
-  nuiMessageData(int16 value)
-  {
-    mType = nuiMessageDataTypeInt16;
-    mValue._int16 = value;
-  }
-
-  nuiMessageData(int32 value)
-  {
-    mType = nuiMessageDataTypeInt32;
-    mValue._int32 = value;
-  }
-
-  nuiMessageData(int64 value)
-  {
-    mType = nuiMessageDataTypeInt64;
-    mValue._int64 = value;
-  }
-
-  nuiMessageData(uint8 value)
-  {
-    mType = nuiMessageDataTypeUInt8;
-    mValue._uint8 = value;
-  }
-
-  nuiMessageData(uint16 value)
-  {
-    mType = nuiMessageDataTypeUInt16;
-    mValue._uint16 = value;
-  }
-
-  nuiMessageData(uint32 value)
-  {
-    mType = nuiMessageDataTypeUInt32;
-    mValue._uint32 = value;
-  }
-
-  nuiMessageData(uint64 value)
-  {
-    mType = nuiMessageDataTypeUInt64;
-    mValue._uint64 = value;
-  }
-
-  nuiMessageData(float value)
-  {
-    mType = nuiMessageDataTypeFloat;
-    mValue._float = value;
-  }
-
-  nuiMessageData(double value)
-  {
-    mType = nuiMessageDataTypeDouble;
-    mValue._double = value;
-  }
-
-  nuiMessageData(nuiMessageDataType type, void* data, size_t size)
-  : mType(type)
-  {
-    switch (mType) {
-      case nuiMessageDataTypeBuffer:
-      {
-        if (!data)
-        {
-          mValue.Pointer._ptr = new uint8[size];
-          mValue.Pointer._size = size;
-          memcpy(mValue.Pointer._ptr, data, size);
-        }
-        else
-        {
-          mValue.Pointer._ptr = nullptr;
-          mValue.Pointer._size = 0;
-        }
-      }break;
-
-      case nuiMessageDataTypeString:
-      {
-        if (data)
-        {
-          mValue.Pointer._ptr = new uint8[size];
-          mValue.Pointer._size = size;
-          memcpy(mValue.Pointer._ptr, data, size);
-        }
-        else
-        {
-          mValue.Pointer._ptr = nullptr;
-          mValue.Pointer._size = 0;
-        }
-      }break;
-
-
-      default:
-      {
-        static uint8 datasizes[] = {
-          sizeof(uint32), //  nuiMessageDataTypeString,
-          sizeof(uint32), //  nuiMessageDataTypeBuffer,
-          sizeof(int8),   //  nuiMessageDataTypeInt8,
-          sizeof(int16),  //  nuiMessageDataTypeInt16,
-          sizeof(int32),  //  nuiMessageDataTypeInt32,
-          sizeof(int64),  //  nuiMessageDataTypeInt64,
-          sizeof(uint8),  //  nuiMessageDataTypeUInt8,
-          sizeof(uint16), //  nuiMessageDataTypeUInt16,
-          sizeof(uint32), //  nuiMessageDataTypeUInt32,
-          sizeof(uint64), //  nuiMessageDataTypeUInt64,
-          sizeof(float),  //  nuiMessageDataTypeFloat,
-          sizeof(double), //  nuiMessageDataTypeDouble
-        };
-
-        memcpy(&mValue, data, datasizes[mType]);
-        NGL_ASSERT(0);
-      }break;
-    }
-
-  }
-
-  nuiMessageData(const nuiMessageData& rData)
-  : mType(rData.mType)
-  {
-    if (rData.mType == nuiMessageDataTypeBuffer || rData.mType == nuiMessageDataTypeString)
-    {
-      mValue.Pointer._ptr = new uint8[rData.mValue.Pointer._size];
-      mValue.Pointer._size = rData.mValue.Pointer._size;
-      memcpy(mValue.Pointer._ptr, rData.mValue.Pointer._ptr, mValue.Pointer._size);
-    }
-    else
-    {
-      mValue = rData.mValue;
-    }
-  }
-
-  nuiMessageData(nuiMessageData&& rData)
-  : mType(rData.mType)
-  {
-    if (rData.mType == nuiMessageDataTypeBuffer || rData.mType == nuiMessageDataTypeString)
-    {
-      mValue.Pointer._ptr = rData.mValue.Pointer._ptr;
-      rData.mValue.Pointer._ptr = nullptr;
-      mValue.Pointer._size = rData.mValue.Pointer._size;
-    }
-    else
-    {
-      mValue = rData.mValue;
-    }
-  }
-
-  nuiMessageData& operator= (nuiMessageData&& rData)
-  {
-    mType = rData.mType;
-    if (rData.mType == nuiMessageDataTypeBuffer || rData.mType == nuiMessageDataTypeString)
-    {
-      mValue.Pointer._ptr = rData.mValue.Pointer._ptr;
-      rData.mValue.Pointer._ptr = nullptr;
-      mValue.Pointer._size = rData.mValue.Pointer._size;
-    }
-    else
-    {
-      mValue = rData.mValue;
-    }
-  }
-
-  ~nuiMessageData()
-  {
-    if (mType == nuiMessageDataTypeBuffer || mType == nuiMessageDataTypeString)
-    {
-      delete[] mValue.Pointer._ptr;
-    }
-  }
+  nuiMessageData(const nglString& rString);
+  nuiMessageData(const void* ptr, size_t size);
+  nuiMessageData(int8 value);
+  nuiMessageData(int16 value);
+  nuiMessageData(int32 value);
+  nuiMessageData(int64 value);
+  nuiMessageData(uint8 value);
+  nuiMessageData(uint16 value);
+  nuiMessageData(uint32 value);
+  nuiMessageData(uint64 value);
+  nuiMessageData(float value);
+  nuiMessageData(double value);
+  nuiMessageData(nuiMessageDataType type, void* data, size_t size);
+  nuiMessageData(const nuiMessageData& rData);
+  nuiMessageData(nuiMessageData&& rData);
+  nuiMessageData& operator= (nuiMessageData&& rData);
+  ~nuiMessageData();
 
 
 
--- src/Introspector/nuiDebugServer.cpp
+++ src/Introspector/nuiDebugServer.cpp
@@ -8,6 +8,214 @@
 
 #include "nuiDebugServer.h"
 
+
+///////////////////////////////// MessageData
+static uint8 datasizes[] = {
+  sizeof(uint32), //  nuiMessageDataTypeString,
+  sizeof(uint32), //  nuiMessageDataTypeBuffer,
+  sizeof(int8),   //  nuiMessageDataTypeInt8,
+  sizeof(int16),  //  nuiMessageDataTypeInt16,
+  sizeof(int32),  //  nuiMessageDataTypeInt32,
+  sizeof(int64),  //  nuiMessageDataTypeInt64,
+  sizeof(uint8),  //  nuiMessageDataTypeUInt8,
+  sizeof(uint16), //  nuiMessageDataTypeUInt16,
+  sizeof(uint32), //  nuiMessageDataTypeUInt32,
+  sizeof(uint64), //  nuiMessageDataTypeUInt64,
+  sizeof(float),  //  nuiMessageDataTypeFloat,
+  sizeof(double), //  nuiMessageDataTypeDouble
+};
+
+nuiMessageData::nuiMessageData(const nglString& rString)
+{
+  mType = nuiMessageDataTypeString;
+  if (rString.IsNull())
+  {
+    mValue.Pointer._ptr = new uint8[rString.GetLength()];
+    mValue.Pointer._size = rString.GetLength();
+    memcpy(mValue.Pointer._ptr, rString.GetChars(), mValue.Pointer._size);
+  }
+  else
+  {
+    mValue.Pointer._ptr = nullptr;
+    mValue.Pointer._size = 0;
+  }
+}
+
+nuiMessageData::nuiMessageData(const void* ptr, size_t size)
+{
+  mType = nuiMessageDataTypeBuffer;
+  if (ptr)
+  {
+    mValue.Pointer._ptr = new uint8[size];
+    mValue.Pointer._size = size;
+    memcpy(mValue.Pointer._ptr, ptr, size);
+  }
+  else
+  {
+    mValue.Pointer._ptr = nullptr;
+    mValue.Pointer._size = 0;
+  }
+}
+
+nuiMessageData::nuiMessageData(int8 value)
+{
+  mType = nuiMessageDataTypeInt8;
+  mValue._int8 = value;
+}
+
+nuiMessageData::nuiMessageData(int16 value)
+{
+  mType = nuiMessageDataTypeInt16;
+  mValue._int16 = value;
+}
+
+nuiMessageData::nuiMessageData(int32 value)
+{
+  mType = nuiMessageDataTypeInt32;
+  mValue._int32 = value;
+}
+
+nuiMessageData::nuiMessageData(int64 value)
+{
+  mType = nuiMessageDataTypeInt64;
+  mValue._int64 = value;
+}
+
+nuiMessageData::nuiMessageData(uint8 value)
+{
+  mType = nuiMessageDataTypeUInt8;
+  mValue._uint8 = value;
+}
+
+nuiMessageData::nuiMessageData(uint16 value)
+{
+  mType = nuiMessageDataTypeUInt16;
+  mValue._uint16 = value;
+}
+
+nuiMessageData::nuiMessageData(uint32 value)
+{
+  mType = nuiMessageDataTypeUInt32;
+  mValue._uint32 = value;
+}
+
+nuiMessageData::nuiMessageData(uint64 value)
+{
+  mType = nuiMessageDataTypeUInt64;
+  mValue._uint64 = value;
+}
+
+nuiMessageData::nuiMessageData(float value)
+{
+  mType = nuiMessageDataTypeFloat;
+  mValue._float = value;
+}
+
+nuiMessageData::nuiMessageData(double value)
+{
+  mType = nuiMessageDataTypeDouble;
+  mValue._double = value;
+}
+
+nuiMessageData::nuiMessageData(nuiMessageDataType type, void* data, size_t size)
+: mType(type)
+{
+  switch (mType) {
+    case nuiMessageDataTypeBuffer:
+    {
+      if (!data)
+      {
+        mValue.Pointer._ptr = new uint8[size];
+        mValue.Pointer._size = size;
+        memcpy(mValue.Pointer._ptr, data, size);
+      }
+      else
+      {
+        mValue.Pointer._ptr = nullptr;
+        mValue.Pointer._size = 0;
+      }
+    }break;
+
+    case nuiMessageDataTypeString:
+    {
+      if (data)
+      {
+        mValue.Pointer._ptr = new uint8[size];
+        mValue.Pointer._size = size;
+        memcpy(mValue.Pointer._ptr, data, size);
+      }
+      else
+      {
+        mValue.Pointer._ptr = nullptr;
+        mValue.Pointer._size = 0;
+      }
+    }break;
+
+
+    default:
+    {
+      memcpy(&mValue, data, datasizes[mType]);
+      NGL_ASSERT(0);
+    }break;
+  }
+
+}
+
+nuiMessageData::nuiMessageData(const nuiMessageData& rData)
+: mType(rData.mType)
+{
+  if (rData.mType == nuiMessageDataTypeBuffer || rData.mType == nuiMessageDataTypeString)
+  {
+    mValue.Pointer._ptr = new uint8[rData.mValue.Pointer._size];
+    mValue.Pointer._size = rData.mValue.Pointer._size;
+    memcpy(mValue.Pointer._ptr, rData.mValue.Pointer._ptr, mValue.Pointer._size);
+  }
+  else
+  {
+    mValue = rData.mValue;
+  }
+}
+
+nuiMessageData::nuiMessageData(nuiMessageData&& rData)
+: mType(rData.mType)
+{
+  if (rData.mType == nuiMessageDataTypeBuffer || rData.mType == nuiMessageDataTypeString)
+  {
+    mValue.Pointer._ptr = rData.mValue.Pointer._ptr;
+    rData.mValue.Pointer._ptr = nullptr;
+    mValue.Pointer._size = rData.mValue.Pointer._size;
+  }
+  else
+  {
+    mValue = rData.mValue;
+  }
+}
+
+nuiMessageData& nuiMessageData::operator= (nuiMessageData&& rData)
+{
+  mType = rData.mType;
+  if (rData.mType == nuiMessageDataTypeBuffer || rData.mType == nuiMessageDataTypeString)
+  {
+    mValue.Pointer._ptr = rData.mValue.Pointer._ptr;
+    rData.mValue.Pointer._ptr = nullptr;
+    mValue.Pointer._size = rData.mValue.Pointer._size;
+  }
+  else
+  {
+    mValue = rData.mValue;
+  }
+}
+
+nuiMessageData::~nuiMessageData()
+{
+  if (mType == nuiMessageDataTypeBuffer || mType == nuiMessageDataTypeString)
+  {
+    delete[] mValue.Pointer._ptr;
+  }
+}
+
+
+
 ///////////////////////////////// MessageClient
 // class nuiMessageClient
 nuiMessageClient::nuiMessageClient(nuiTCPClient* pTCPClient)
@@ -114,21 +322,6 @@ nuiMessage* nuiMessageClient::Read()
 {
   return nullptr;
 }
-
-static uint8 datasizes[] = {
-sizeof(uint32), //  nuiMessageDataTypeString,
-sizeof(uint32), //  nuiMessageDataTypeBuffer,
-sizeof(int8),   //  nuiMessageDataTypeInt8,
-sizeof(int16),  //  nuiMessageDataTypeInt16,
-sizeof(int32),  //  nuiMessageDataTypeInt32,
-sizeof(int64),  //  nuiMessageDataTypeInt64,
-sizeof(uint8),  //  nuiMessageDataTypeUInt8,
-sizeof(uint16), //  nuiMessageDataTypeUInt16,
-sizeof(uint32), //  nuiMessageDataTypeUInt32,
-sizeof(uint64), //  nuiMessageDataTypeUInt64,
-sizeof(float),  //  nuiMessageDataTypeFloat,
-sizeof(double), //  nuiMessageDataTypeDouble
-};
 
 void FixEndianness(nuiMessageDataType type, std::vector<uint8>& rData)
 {
