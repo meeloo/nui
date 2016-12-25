52f99f45cef82391bec3926590775ca739dfc6ca
Sébastien Métrot <smetrot@apple.com>
2016-12-25 02:20:04 +0100

    better 64bit detection

--- include/ngl.h
+++ include/ngl.h
@@ -31,7 +31,7 @@ and in NGL user application code.
  * Platform detection
  */
 
-#if (defined __X86_64__) || (defined _M_X64) || (defined __ppc64__) || (defined __LP64__)
+#if (defined __X86_64__) || (defined _M_X64) || (defined __ppc64__) || (defined __LP64__) || (defined __aarch64__)
 // 64bits platform
 #define __NUI64__
 #else
